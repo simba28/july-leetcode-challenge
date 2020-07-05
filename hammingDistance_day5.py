723efe990b46cc9fd55b037a52497a393ea7af58
Sudhanshu Kumar <sudh2810@gmail.com>
2020-07-05 20:12:43 +0530

    day 5 code using xor and right shift

--- hammingDistance_day5.py
+++ hammingDistance_day5.py
@@ -1,25 +1,38 @@
 class Solution:
     def hammingDistance(self, x: int, y: int) -> int:
         
-        a = list(bin(x)[2:])
-        b = list(bin(y)[2:])
+        # a = list(bin(x)[2:])
+        # b = list(bin(y)[2:])
         
-        la = len(a)
-        lb = len(b)
+        # la = len(a)
+        # lb = len(b)
         
-        if la > lb:
-            b = (['0'] * (la-lb)) + b
-        else:
-            a = (['0'] * (lb-la)) + a
+        # if la > lb:
+        #     b = (['0'] * (la-lb)) + b
+        # else:
+        #     a = (['0'] * (lb-la)) + a
             
-        count = 0
+        # count = 0
         
         
-        for i in range(len(a)):
-            if a[i]!=b[i]:
-                count += 1
+        # for i in range(len(a)):
+        #     if a[i]!=b[i]:
+        #         count += 1
                 
+        # return count
+
+        # more efficiently
+
+        result = x ^ y #xor operation
+
+        count = 0
+        while result>0 :
+            count += result & 1  # to check if the last bit is same as 1 or not
+            result = result>>1
+        
         return count
+
+
 
 # test = Solution()
 # print(test.hammingDistance(4,1))
